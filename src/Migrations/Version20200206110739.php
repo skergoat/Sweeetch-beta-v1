6b10b7a7435c9d951d4e2205359106b5f2dec3de
skergoat <skergoatweb@gmail.com>
2020-02-06 18:08:34 +0700

    end public file system

---
+++ src/Migrations/Version20200206110739.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200206110739 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF33D262AF09');
+        $this->addSql('DROP INDEX UNIQ_B723AF33D262AF09 ON student');
+        $this->addSql('ALTER TABLE student DROP resume_id');
+        $this->addSql('ALTER TABLE resume CHANGE url url VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE resume CHANGE url url VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE student ADD resume_id INT NOT NULL');
+        $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33D262AF09 FOREIGN KEY (resume_id) REFERENCES resume (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_B723AF33D262AF09 ON student (resume_id)');
+    }
+}
