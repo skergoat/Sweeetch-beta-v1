0f0468fc4db2c8842eafb382bae9b2f1e04db83d
skergoat <skergoatweb@gmail.com>
2020-02-10 06:20:26 +0700

    offers and fixturess

--- src/DataFixtures/CompanyFixtures.php
+++ src/DataFixtures/CompanyFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Offers;
 use App\Entity\Company;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -37,10 +38,33 @@ class CompanyFixtures extends Fixture
             'company'
         ));
 
+            $offer1 = new Offers;
+            $offer1->setTitle('developpeur front-end'); 
+            $offer1->setDomain('informatique');
+            $offer1->setLocation('Paris'); 
+            $offer1->setDateStart(new \DateTime('now')); 
+            $offer1->setDateEnd(new \DateTime('now'));
+            $offer1->setDescription('Un super bon job !'); 
+
+            $offer2 = new Offers;
+            $offer2->setTitle('developpeur front-end'); 
+            $offer2->setDomain('informatique');
+            $offer2->setLocation('Paris'); 
+            $offer2->setDateStart(new \DateTime('now')); 
+            $offer2->setDateEnd(new \DateTime('now'));
+            $offer2->setDescription('Un super bon job !'); 
+
+            $offer1->setCompany($ca);
+            $offer2->setCompany($ca);
+
+            $manager->persist($offer1);
+            $manager->persist($offer2);
+
         $ca->setUser($user);
 
         $manager->persist($ca);
         $manager->persist($user);
+
         $manager->flush();
     }
 }
---
+++ src/Entity/Offers.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OffersRepository")
+ */
+class Offers
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $domain;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateStart;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateEnd;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Company", inversedBy="offers")
+     */
+    private $company;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getDomain(): ?string
+    {
+        return $this->domain;
+    }
+
+    public function setDomain(string $domain): self
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    public function getDateStart(): ?\DateTimeInterface
+    {
+        return $this->dateStart;
+    }
+
+    public function setDateStart(\DateTimeInterface $dateStart): self
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    public function getDateEnd(): ?\DateTimeInterface
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(\DateTimeInterface $dateEnd): self
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+}
