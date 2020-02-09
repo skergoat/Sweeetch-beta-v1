4c0474e98986ce67fd809e07dbb17a88afa28a9b
skergoat <skergoatweb@gmail.com>
2020-02-10 05:13:04 +0700

    some changes company

--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -14,6 +14,7 @@ class CompanyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('companyName', TextType::class)
             ->add('firstname', TextType::class)
             ->add('lastname', TextType::class)
             ->add('address', TextType::class)
