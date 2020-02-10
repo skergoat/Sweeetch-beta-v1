686d5aed5013443321848581a151e1ea6e22bf0c
skergoat <skergoatweb@gmail.com>
2020-02-10 07:53:51 +0700

    formate template offers

--- src/Form/OffersType.php
+++ src/Form/OffersType.php
@@ -6,18 +6,25 @@ use App\Entity\Offers;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class OffersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('location')
-            ->add('domain')
-            ->add('dateStart')
-            ->add('dateEnd')
-            ->add('description')
+            ->add('title', TextType::class)
+            ->add('location', TextType::class)
+            ->add('domain', TextType::class)
+            ->add('dateStart', DateTimeType::Class, [
+                'widget' => 'single_text'
+            ])
+            ->add('dateEnd', DateTimeType::Class, [
+                'widget' => 'single_text'
+            ])
+            ->add('description', TextareaType::class)
             // ->add('company')
         ;
     }
