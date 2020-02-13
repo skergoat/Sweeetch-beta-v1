7b613f17271f51c43def37dcecae89fcd7336c63
skergoat <skergoatweb@gmail.com>
2020-02-13 09:41:35 +0700

    refuse, delete ok

--- src/Controller/Company/CompanyController.php
+++ src/Controller/Company/CompanyController.php
@@ -121,6 +121,16 @@ class CompanyController extends AbstractController
                         "ROLE_TO_APPLY"
                     ]);
 
+                     // set other student offers to unavailable
+                    // $unavailables = $repository->setToUnavailables($offers, $student);
+
+                    // foreach($unavailables as $unavailables) {
+
+                    //     if($unavailables->getUnavailable() == true) {
+                    //         $unavailables->setUnavailable(false);
+                    //     } 
+                    // }
+
                     // send mail 
                     $email = $student->getUser()->getEmail();
                     $name = $student->getName();
