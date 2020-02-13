33480c051e3fdc888ebe41b1affcf0be69e921fe
skergoat <skergoatweb@gmail.com>
2020-02-13 09:33:12 +0700

    system applies almost ok

--- src/Migrations/Version20200213022011.php
+++ src/Migrations/Version20200213022011.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200213014545 extends AbstractMigration
+final class Version20200213022011 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,10 @@ final class Version20200213014545 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE apply (id INT AUTO_INCREMENT NOT NULL, offers_id INT NOT NULL, student_id INT NOT NULL, hired TINYINT(1) NOT NULL, confirmed TINYINT(1) NOT NULL, refused TINYINT(1) NOT NULL, unavailable TINYINT(1) NOT NULL, INDEX IDX_BD2F8C1FA090B42E (offers_id), INDEX IDX_BD2F8C1FCB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE studies (id INT AUTO_INCREMENT NOT NULL, school_id INT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_C3A91A3FC32A47EE (school_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE school (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, zip_code VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, tel_number VARCHAR(255) NOT NULL, siret VARCHAR(255) NOT NULL, company_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_F99EDABBA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE id_card (id INT AUTO_INCREMENT NOT NULL, file_name VARCHAR(255) DEFAULT NULL, original_filename VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE apply (id INT AUTO_INCREMENT NOT NULL, offers_id INT NOT NULL, student_id INT NOT NULL, hired TINYINT(1) DEFAULT NULL, confirmed TINYINT(1) DEFAULT NULL, refused TINYINT(1) DEFAULT NULL, INDEX IDX_BD2F8C1FA090B42E (offers_id), INDEX IDX_BD2F8C1FCB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE resume (id INT AUTO_INCREMENT NOT NULL, file_name VARCHAR(255) DEFAULT NULL, original_filename VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, profile_id INT DEFAULT NULL, resume_id INT NOT NULL, id_card_id INT NOT NULL, student_card_id INT NOT NULL, proof_habitation_id INT NOT NULL, name VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, zip_code VARCHAR(255) NOT NULL, tel_number VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, driving_license TINYINT(1) DEFAULT NULL, disabled TINYINT(1) DEFAULT NULL, UNIQUE INDEX UNIQ_B723AF33A76ED395 (user_id), UNIQUE INDEX UNIQ_B723AF33CCFA12B8 (profile_id), UNIQUE INDEX UNIQ_B723AF33D262AF09 (resume_id), UNIQUE INDEX UNIQ_B723AF3394513350 (id_card_id), UNIQUE INDEX UNIQ_B723AF33A7FA2FD8 (student_card_id), UNIQUE INDEX UNIQ_B723AF3360E93A41 (proof_habitation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,10 +36,10 @@ final class Version20200213014545 extends AbstractMigration
         $this->addSql('CREATE TABLE education (id INT AUTO_INCREMENT NOT NULL, profile_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, school VARCHAR(255) NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME DEFAULT NULL, current TINYINT(1) DEFAULT NULL, INDEX IDX_DB0A5ED2CCFA12B8 (profile_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE student_card (id INT AUTO_INCREMENT NOT NULL, file_name VARCHAR(255) NOT NULL, original_filename VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE offers (id INT AUTO_INCREMENT NOT NULL, company_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, domain VARCHAR(255) NOT NULL, date_start DATETIME NOT NULL, date_end DATETIME NOT NULL, description LONGTEXT NOT NULL, INDEX IDX_DA460427979B1AD6 (company_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE studies ADD CONSTRAINT FK_C3A91A3FC32A47EE FOREIGN KEY (school_id) REFERENCES school (id)');
-        $this->addSql('ALTER TABLE school ADD CONSTRAINT FK_F99EDABBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE apply ADD CONSTRAINT FK_BD2F8C1FA090B42E FOREIGN KEY (offers_id) REFERENCES offers (id)');
         $this->addSql('ALTER TABLE apply ADD CONSTRAINT FK_BD2F8C1FCB944F1A FOREIGN KEY (student_id) REFERENCES student (id)');
+        $this->addSql('ALTER TABLE studies ADD CONSTRAINT FK_C3A91A3FC32A47EE FOREIGN KEY (school_id) REFERENCES school (id)');
+        $this->addSql('ALTER TABLE school ADD CONSTRAINT FK_F99EDABBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33CCFA12B8 FOREIGN KEY (profile_id) REFERENCES profile (id)');
         $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33D262AF09 FOREIGN KEY (resume_id) REFERENCES resume (id)');
@@ -71,10 +71,10 @@ final class Version20200213014545 extends AbstractMigration
         $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF3360E93A41');
         $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF33A7FA2FD8');
         $this->addSql('ALTER TABLE apply DROP FOREIGN KEY FK_BD2F8C1FA090B42E');
+        $this->addSql('DROP TABLE apply');
         $this->addSql('DROP TABLE studies');
         $this->addSql('DROP TABLE school');
         $this->addSql('DROP TABLE id_card');
-        $this->addSql('DROP TABLE apply');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE resume');
         $this->addSql('DROP TABLE student');
