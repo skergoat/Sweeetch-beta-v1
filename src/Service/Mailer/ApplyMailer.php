0c185dabb4249ef454b9918c7b45bc7bdbf926ac
skergoat <skergoatweb@gmail.com>
2020-02-14 14:13:17 +0700

    some mail

--- src/Service/Mailer/ApplyMailer.php
+++ src/Service/Mailer/ApplyMailer.php
@@ -16,6 +16,36 @@ class ApplyMailer
         $this->mailer = $mailer;
     }
 
+    public function sendApplyMessage($email, $name, $title)
+    {
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email, $name))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/apply/apply.html.twig')
+            ->context([
+                'title' => $title,
+            ]); 
+        
+        $this->mailer->send($mail);
+    }
+
+    public function sendHireMessage($email, $name, $title)
+    {
+        // dd('send');
+
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email, $name))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/apply/hire.html.twig')
+            ->context([
+                'title' => $title,
+            ]); 
+        
+        $this->mailer->send($mail);
+    }
+
     public function sendOthersMessage($email, $name, $title)
     {
         $mail = (new TemplatedEmail())
@@ -30,21 +60,7 @@ class ApplyMailer
         $this->mailer->send($mail);
     }
 
-    public function sendDeleteMessage($email, $name, $title)
-    {
-        $mail = (new TemplatedEmail())
-            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
-            ->to(new Address($email, $name))
-            ->subject('Problems with docs')
-            ->htmlTemplate('email/apply/delete.html.twig')
-            ->context([
-                'title' => $title,
-            ]); 
-        
-        $this->mailer->send($mail);
-    }
-
-    public function sendDeleteCompanyMessage($email, $name, $title)
+    public function sendAgreeMessage($email, $name, $title)
     {
         // dd('send');
 
@@ -52,7 +68,53 @@ class ApplyMailer
             ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
             ->to(new Address($email, $name))
             ->subject('Problems with docs')
-            ->htmlTemplate('email/apply/delete-company.html.twig')
+            ->htmlTemplate('email/apply/agree.html.twig')
+            ->context([
+                'title' => $title,
+            ]); 
+        
+        $this->mailer->send($mail);
+    }
+
+    public function sendConfirmMessage($email, $name, $title)
+    {
+        // dd('send');
+
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email, $name))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/apply/confirm.html.twig')
+            ->context([
+                'title' => $title,
+            ]); 
+        
+        $this->mailer->send($mail);
+    }
+
+    public function sendFinishMessage($email, $name, $title)
+    {
+        // dd('send');
+
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email, $name))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/apply/finish.html.twig')
+            ->context([
+                'title' => $title,
+            ]); 
+        
+        $this->mailer->send($mail);
+    }
+
+    public function sendDeleteMessage($email, $name, $title)
+    {
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email, $name))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/apply/delete.html.twig')
             ->context([
                 'title' => $title,
             ]); 
