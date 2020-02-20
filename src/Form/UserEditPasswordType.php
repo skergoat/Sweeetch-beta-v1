0a753422dc85a24f6e3c06bab603f12f86b94ccd
skergoat <skergoatweb@gmail.com>
2020-02-20 14:27:21 +0700

    constraints

---
+++ src/Form/UserEditPasswordType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserEditPasswordType extends UserType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('password', PasswordType::Class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
