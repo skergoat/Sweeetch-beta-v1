4e76249f3f08a07bdc932ae84186d8d2d0ef31ba
skergoat <skergoatweb@gmail.com>
2020-02-21 08:37:36 +0700

    area choices

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class ProfileType extends AbstractType
@@ -17,7 +18,30 @@ class ProfileType extends AbstractType
     {
         $builder
             ->add('domain', TextType::class)
-            ->add('area', TextType::class)
+            // ->add('area', TextType::class)
+
+            ->add('area', ChoiceType::class, [
+                'choices' => [
+                    'Auvergne-Rhône-Alpes' => 'Auvergne-Rhône-Alpes',
+                    'Bourgogne-Franche-Comté' => 'Bourgogne-Franche-Comté',
+                    'Bretagne' => 'Bretagne',
+                    'Centre-Val de Loire' => 'Centre-Val de Loire',
+                    'Corse' => 'Corse',
+                    'Grand Est' => 'Grand Est',
+                    'Hauts-de-France' => 'Hauts-de-France',
+                    'Île-de-France' => 'Île-de-France',
+                    'Normandie' => 'Normandie',
+                    'Nouvelle-Aquitaine' => 'Nouvelle-Aquitaine',
+                    'Occitanie' => 'Occitanie',
+                    'Pays de la Loire' => 'Pays de la Loire', 
+                    'Provence-Alpes-Côte d\'Azur' => 'Provence-Alpes-Côte d\'Azur'
+                ],
+                'choice_attr' => function($choice, $key, $value) {
+                    // adds a class like attending_yes, attending_no, etc
+                    return ['class' => 'attending_'.strtolower($key)];
+                },
+            ])
+
             ->add('languages', CollectionType::class, array(
                 'entry_type' => LanguageType::class,
                 'allow_delete' => true,
