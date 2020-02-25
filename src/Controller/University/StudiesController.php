87ced5d96a87313166bd225f209198aa79ba1f29
skergoat <skergoatweb@gmail.com>
2020-02-25 11:38:24 +0700

    secured school and studies

--- src/Controller/University/StudiesController.php
+++ src/Controller/University/StudiesController.php
@@ -9,11 +9,13 @@ use App\Repository\StudiesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/studies")
+ * @IsGranted("ROLE_SUPER_ADMIN")
  */
 class StudiesController extends AbstractController
 {
