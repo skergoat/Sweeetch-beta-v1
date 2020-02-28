fac71a65f0c87e25aaeac764e1bed66796bae241
skergoat <skergoatweb@gmail.com>
2020-02-28 13:48:16 +0700

    debut mise en ligne et mainteance debug 0

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Corley\MaintenanceBundle\CorleyMaintenanceBundle::class => ['all' => true],
 ];
