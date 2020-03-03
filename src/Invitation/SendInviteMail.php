72ea39e0df1ab2ab8ded2aa53e9851d010196de8
skergoat <skergoatweb@gmail.com>
2020-03-03 13:31:27 +0700

    installation

---
+++ src/Invitation/SendInviteMail.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Invitation;
+
+class SendInviteMail
+{
+    private $content;
+
+    public function __construct(string $content)
+    {
+        $this->content = $content;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+}
