ae523437cf851de0b03ed82247abea475ca781cc
skergoat <skergoatweb@gmail.com>
2020-03-03 13:31:27 +0700

    installation

---
+++ src/Service/Mailer/InviteMailer.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace App\Service\Mailer;
+
+use App\Entity\User;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class InviteMailer
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function invite($email)
+    {
+        $mail = (new TemplatedEmail())
+            ->from(new Address('no-reply@sweeetch.com', 'Sweeetch\'s Team'))
+            ->to(new Address($email))
+            ->subject('Problems with docs')
+            ->htmlTemplate('email/invite.html.twig');
+        
+        $this->mailer->send($mail);
+    }
+}
