389c916c67c1b2d9000490d811ff59148d9c1b2a
skergoat <skergoatweb@gmail.com>
2020-03-03 08:01:14 +0700

    files

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Invitation\SendInviteMail;
+use App\Service\Mailer\InviteMailer;
 use App\Service\Mailer\ContactMailer;
+use App\Repository\InvitationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FrontController extends AbstractController
@@ -34,4 +39,35 @@ class FrontController extends AbstractController
         return $this->redirectToRoute('homepage');
     }
 
+    //  /**
+    //  * @Route("/invite", name="invite")
+    //  * @IsGranted("ROLE_ADMIN")
+    //  */
+    // public function invitation(InvitationRepository $repository,  InviteMailer $mailer) 
+    // {
+    //     $mail = $repository->findAll();
+
+    //     foreach($mail as $mail)
+    //     {
+    //         $mailer->invite($mail->getEmails());
+    //     }
+
+    //     $this->addFlash('success', 'Invitations envoyées');
+
+    //     return $this->redirectToRoute('admin');
+    // }
+
+     /**
+     * @Route("/invite", name="invite")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function invite(MessageBusInterface $bus)
+    {
+        // or use the shortcut
+        $this->dispatchMessage(new SendInviteMail('sfsfsdfs'));
+
+        $this->addFlash('success', 'Invitations envoyées');
+
+        return $this->redirectToRoute('admin');
+    }
 }
