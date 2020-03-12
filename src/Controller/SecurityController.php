110ff9a26c220173f529b9e9b37d206a6cb91f2e
skergoat <skergoatweb@gmail.com>
2020-03-12 15:35:37 +0700

    env-2

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -65,72 +65,72 @@ class SecurityController extends AbstractController
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 
-    /**
-     * @Route("/oubli-pass", name="app_forgotten_password")
-     */
-    public function oubliPass(Request $request, UsersRepository $users, TokenGeneratorInterface $tokenGenerator): Response
-    {
-        // On initialise le formulaire
-        $form = $this->createForm(ResetPassType::class);
+    // /**
+    //  * @Route("/oubli-pass", name="app_forgotten_password")
+    //  */
+    // public function oubliPass(Request $request, UsersRepository $users, TokenGeneratorInterface $tokenGenerator): Response
+    // {
+    //     // On initialise le formulaire
+    //     $form = $this->createForm(ResetPassType::class);
 
-        // On traite le formulaire
-        $form->handleRequest($request);
+    //     // On traite le formulaire
+    //     $form->handleRequest($request);
 
-        // // Si le formulaire est valide
-        // if ($form->isSubmitted() && $form->isValid()) {
-        //     // On récupère les données
-        //     $donnees = $form->getData();
+    //     // // Si le formulaire est valide
+    //     // if ($form->isSubmitted() && $form->isValid()) {
+    //     //     // On récupère les données
+    //     //     $donnees = $form->getData();
 
-        //     // On cherche un utilisateur ayant cet e-mail
-        //     $user = $users->findOneByEmail($donnees['email']);
+    //     //     // On cherche un utilisateur ayant cet e-mail
+    //     //     $user = $users->findOneByEmail($donnees['email']);
 
-        //     // Si l'utilisateur n'existe pas
-        //     if ($user === null) {
-        //         // On envoie une alerte disant que l'adresse e-mail est inconnue
-        //         $this->addFlash('danger', 'Cette adresse e-mail est inconnue');
+    //     //     // Si l'utilisateur n'existe pas
+    //     //     if ($user === null) {
+    //     //         // On envoie une alerte disant que l'adresse e-mail est inconnue
+    //     //         $this->addFlash('danger', 'Cette adresse e-mail est inconnue');
                 
-        //         // On retourne sur la page de connexion
-        //         return $this->redirectToRoute('app_login');
-        //     }
+    //     //         // On retourne sur la page de connexion
+    //     //         return $this->redirectToRoute('app_login');
+    //     //     }
 
-        //     // On génère un token
-        //     $token = $tokenGenerator->generateToken();
+    //     //     // On génère un token
+    //     //     $token = $tokenGenerator->generateToken();
 
-        //     // On essaie d'écrire le token en base de données
-        //     try{
-        //         $user->setResetToken($token);
-        //         $entityManager = $this->getDoctrine()->getManager();
-        //         $entityManager->persist($user);
-        //         $entityManager->flush();
-        //     } catch (\Exception $e) {
-        //         $this->addFlash('warning', $e->getMessage());
-        //         return $this->redirectToRoute('app_login');
-        //     }
+    //     //     // On essaie d'écrire le token en base de données
+    //     //     try{
+    //     //         $user->setResetToken($token);
+    //     //         $entityManager = $this->getDoctrine()->getManager();
+    //     //         $entityManager->persist($user);
+    //     //         $entityManager->flush();
+    //     //     } catch (\Exception $e) {
+    //     //         $this->addFlash('warning', $e->getMessage());
+    //     //         return $this->redirectToRoute('app_login');
+    //     //     }
 
-        //     // On génère l'URL de réinitialisation de mot de passe
-        //     $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
+    //     //     // On génère l'URL de réinitialisation de mot de passe
+    //     //     $url = $this->generateUrl('app_reset_password', array('token' => $token), UrlGeneratorInterface::ABSOLUTE_URL);
 
-        //     // On génère l'e-mail
-        //     $message = (new \Swift_Message('Mot de passe oublié'))
-        //         ->setFrom('votre@adresse.fr')
-        //         ->setTo($user->getEmail())
-        //         ->setBody(
-        //             "Bonjour,<br><br>Une demande de réinitialisation de mot de passe a été effectuée pour le site Nouvelle-Techno.fr. Veuillez cliquer sur le lien suivant : " . $url,
-        //             'text/html'
-        //         )
-        //     ;
+    //     //     // On génère l'e-mail
+    //     //     $message = (new \Swift_Message('Mot de passe oublié'))
+    //     //         ->setFrom('votre@adresse.fr')
+    //     //         ->setTo($user->getEmail())
+    //     //         ->setBody(
+    //     //             "Bonjour,<br><br>Une demande de réinitialisation de mot de passe a été effectuée pour le site Nouvelle-Techno.fr. Veuillez cliquer sur le lien suivant : " . $url,
+    //     //             'text/html'
+    //     //         )
+    //     //     ;
 
-        //     // On envoie l'e-mail
-        //     $mailer->send($message);
+    //     //     // On envoie l'e-mail
+    //     //     $mailer->send($message);
 
-        //     // On crée le message flash de confirmation
-        //     $this->addFlash('message', 'E-mail de réinitialisation du mot de passe envoyé !');
+    //     //     // On crée le message flash de confirmation
+    //     //     $this->addFlash('message', 'E-mail de réinitialisation du mot de passe envoyé !');
 
-        //     // On redirige vers la page de login
-        //     return $this->redirectToRoute('app_login');
-        // }
+    //     //     // On redirige vers la page de login
+    //     //     return $this->redirectToRoute('app_login');
+    //     // }
 
-        // On envoie le formulaire à la vue
-        return $this->render('security/forgotten_password.html.twig',['emailForm' => $form->createView()]);
-    }
+    //     // On envoie le formulaire à la vue
+    //     return $this->render('security/forgotten_password.html.twig',['emailForm' => $form->createView()]);
+    // }
 }
