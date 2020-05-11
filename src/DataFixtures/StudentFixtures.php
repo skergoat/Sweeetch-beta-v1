775045fb2e84e73fec1ced3aa76678600bedf160
skergoat <skergoatweb@gmail.com>
2020-05-11 13:00:20 +0700

    fixtures

--- src/DataFixtures/StudentFixtures.php
+++ src/DataFixtures/StudentFixtures.php
@@ -27,96 +27,96 @@ class StudentFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        for($i = 1 ; $i < 10 ; $i++) {
+        // for($i = 1 ; $i < 10 ; $i++) {
 
-            $student = new Student;
-            $student->setName('Marc' . $i);
-            $student->setLastname('Dupont' . $i);
-            $student->setAdress($i . ' rue du chemin vert');
-            $student->setZipCode('7' . $i . '500');
-            $student->setTelNumber('0' . $i . '.89.78.67.43');
-            $student->setCity('Paris');
+        //     $student = new Student;
+        //     $student->setName('Marc' . $i);
+        //     $student->setLastname('Dupont' . $i);
+        //     $student->setAdress($i . ' rue du chemin vert');
+        //     $student->setZipCode('7' . $i . '500');
+        //     $student->setTelNumber('0' . $i . '.89.78.67.43');
+        //     $student->setCity('Paris');
 
-            $user = new User; 
-            $user->setEmail('crypte' . $i . '@gmail.com');
-            $user->setRoles(['ROLE_STUDENT']);
-            $user->setConfirmed(false);
-            $user->setPassword($this->passwordEncoder->encodePassword(
-                $user,
-                'crypte'
-            ));
+        //     $user = new User; 
+        //     $user->setEmail('crypte' . $i . '@gmail.com');
+        //     $user->setRoles(['ROLE_STUDENT']);
+        //     $user->setConfirmed(false);
+        //     $user->setPassword($this->passwordEncoder->encodePassword(
+        //         $user,
+        //         'crypte'
+        //     ));
 
-            $profile = new Profile;
-            $profile->setDomain('Développement web');
-            $profile->setArea('Normandie');
+        //     $profile = new Profile;
+        //     $profile->setDomain('Développement web');
+        //     $profile->setArea('Normandie');
             
-                $language1 = new Language;
-                $language1->setLanguageName('Anglais'); 
-                $language1->setLevel('B2'); 
+        //         $language1 = new Language;
+        //         $language1->setLanguageName('Anglais'); 
+        //         $language1->setLevel('B2'); 
 
-                $language2 = new Language;
-                $language2->setLanguageName('Vietnamien'); 
-                $language2->setLevel('B2'); 
+        //         $language2 = new Language;
+        //         $language2->setLanguageName('Vietnamien'); 
+        //         $language2->setLevel('B2'); 
 
-                $date = new \Datetime('2019-03-06');
-                $date2 = new \Datetime('2020-04-06');
+        //         $date = new \Datetime('2019-03-06');
+        //         $date2 = new \Datetime('2020-04-06');
 
-                $education1 = new Education;
-                $education1->setTitle('Baccalaureat general - S'); 
-                $education1->setSchool('Lycee du Bon Pasteur');
-                $education1->setDateStart($date);
-                $education1->setDateEnd($date2); 
-                $education1->setCurrent(false); 
+        //         $education1 = new Education;
+        //         $education1->setTitle('Baccalaureat general - S'); 
+        //         $education1->setSchool('Lycee du Bon Pasteur');
+        //         $education1->setDateStart($date);
+        //         $education1->setDateEnd($date2); 
+        //         $education1->setCurrent(false); 
 
-                $education2 = new Education;
-                $education2->setTitle('L1 developement web'); 
-                $education2->setSchool('Universite de Paris'); 
-                $education2->setDateStart($date);
-                $education2->setDateEnd($date2);  
-                $education2->setCurrent(false);
+        //         $education2 = new Education;
+        //         $education2->setTitle('L1 developement web'); 
+        //         $education2->setSchool('Universite de Paris'); 
+        //         $education2->setDateStart($date);
+        //         $education2->setDateEnd($date2);  
+        //         $education2->setCurrent(false);
 
-                $language1->setProfile($profile);
-                $language2->setProfile($profile);
+        //         $language1->setProfile($profile);
+        //         $language2->setProfile($profile);
 
-                $education1->setProfile($profile);
-                $education2->setProfile($profile);
+        //         $education1->setProfile($profile);
+        //         $education2->setProfile($profile);
 
-                $manager->persist($language1);
-                $manager->persist($language2);
+        //         $manager->persist($language1);
+        //         $manager->persist($language2);
 
-                $manager->persist($education1);
-                $manager->persist($education2);
+        //         $manager->persist($education1);
+        //         $manager->persist($education2);
             
-            $resume = new Resume;
-            $resume->setFileName('team-1-5e4e6ea8d954f.jpeg');
-            $resume->setOriginalFilename('team-1-5e4e6ea8d954f.jpeg');
-            $resume->setMimeType('image/jpeg');
+        //     $resume = new Resume;
+        //     $resume->setFileName('team-1-5e4e6ea8d954f.jpeg');
+        //     $resume->setOriginalFilename('team-1-5e4e6ea8d954f.jpeg');
+        //     $resume->setMimeType('image/jpeg');
 
-            $IdCard = new IdCard;
-            $IdCard->setFileName('team-2-5e4e6ed8d830f.jpeg');
-            $IdCard->setOriginalFilename('team-2-5e4e6ed8d830f.jpeg');
-            $IdCard->setMimeType('image/jpeg');
+        //     $IdCard = new IdCard;
+        //     $IdCard->setFileName('team-2-5e4e6ed8d830f.jpeg');
+        //     $IdCard->setOriginalFilename('team-2-5e4e6ed8d830f.jpeg');
+        //     $IdCard->setMimeType('image/jpeg');
 
-            $studentCard = new StudentCard;
-            $studentCard->setFileName('team-3-5e4e6ece85a37.jpeg');
-            $studentCard->setOriginalFilename('team-3-5e4e6ece85a37.jpeg');
-            $studentCard->setMimeType('image/jpeg');
+        //     $studentCard = new StudentCard;
+        //     $studentCard->setFileName('team-3-5e4e6ece85a37.jpeg');
+        //     $studentCard->setOriginalFilename('team-3-5e4e6ece85a37.jpeg');
+        //     $studentCard->setMimeType('image/jpeg');
 
-            $proofHabitation = new ProofHabitation;
-            $proofHabitation->setFileName('team-4-5e4e6ec479d8a.jpeg');
-            $proofHabitation->setOriginalFilename('team-4-5e4e6ec479d8a.jpeg');
-            $proofHabitation->setMimeType('image/jpeg');
+        //     $proofHabitation = new ProofHabitation;
+        //     $proofHabitation->setFileName('team-4-5e4e6ec479d8a.jpeg');
+        //     $proofHabitation->setOriginalFilename('team-4-5e4e6ec479d8a.jpeg');
+        //     $proofHabitation->setMimeType('image/jpeg');
 
-            $student->setUser($user);
-            $student->setProfile($profile);
-            $student->setResume($resume);
-            $student->setIdCard($IdCard);
-            $student->setStudentCard($studentCard);
-            $student->setProofHabitation($proofHabitation);
+        //     $student->setUser($user);
+        //     $student->setProfile($profile);
+        //     $student->setResume($resume);
+        //     $student->setIdCard($IdCard);
+        //     $student->setStudentCard($studentCard);
+        //     $student->setProofHabitation($proofHabitation);
 
-            $manager->persist($user);
-            $manager->persist($student);
-        }
+        //     $manager->persist($user);
+        //     $manager->persist($student);
+        // }
 
         $manager->flush();
     }
