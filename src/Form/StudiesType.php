47423b66779cdc79e1796bea4c026418f4409442
skergoat <skergoatweb@gmail.com>
2020-06-23 10:25:25 +0700

    domain

--- src/Form/StudiesType.php
+++ src/Form/StudiesType.php
@@ -48,21 +48,13 @@ class StudiesType extends AbstractType
             ])
             ->add('domain', ChoiceType::class, [
                 'choices' => [
-                    'Administration & législation' => 'Administration & législation',
-                    'Bâtiment & construction' => 'Bâtiment & construction',
-                    'Communication' => 'Communication',
-                    'Culture' => 'Culture',
-                    'Economie & gestion' => 'Economie & gestion',
-                    'Environnement & nature' => 'Environnement & nature',
-                    'Hôtellerie & alimentation' => 'Hôtellerie & alimentation',
-                    'Informatique & télécommunication' => 'Informatique & télécommunication',
-                    'Santé & bien-être' => 'Santé & bien-être',
-                    'Sciences' => 'Sciences',
-                    'Sciences humaines & sociales' => 'Sciences humaines & sociales',
-                    'Sécurité' => 'Sécurité', 
-                    'Technique & industrie' => 'Technique & industrie',
-                    'Tourisme, sports & loisirs' => 'Tourisme, sports & loisirs',
-                    'Transports & logistique' => 'Transports & logistique'
+                    'Administration & législation' => 'Grande distribution',
+                    'Bâtiment & construction' => 'Vente & Commerce',
+                    'Communication' => 'Restauration',
+                    'Culture' => 'Artisanat',
+                    'Economie & gestion' => 'Marketing & Communication',
+                    'Environnement & nature' => 'Assistanat & secrétariat',
+                    'Hôtellerie & alimentation' => 'Immobilier',
                 ],
                 'choice_attr' => function($choice, $key, $value) {
                     return ['class' => 'attending_'.strtolower($key)];
