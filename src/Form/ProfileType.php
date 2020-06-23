72da14967f5b65d8dfd626e686b2168e719a59ef
skergoat <skergoatweb@gmail.com>
2020-06-23 10:25:25 +0700

    domain

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -20,21 +20,13 @@ class ProfileType extends AbstractType
 
             ->add('domain', ChoiceType::class, [
                 'choices' => [
-                    'Administration & législation' => 'Administration & législation',
-                    'Bâtiment & construction' => 'Bâtiment & construction',
-                    'Communication' => 'Communication',
-                    'Culture' => 'Culture',
-                    'Economie & gestion' => 'Economie & gestion',
-                    'Environnement & nature' => 'Environnement & nature',
-                    'Hôtellerie & alimentation' => 'Hôtellerie & alimentation',
-                    'Informatique & télécommunication' => 'Informatique & télécommunication',
-                    'Santé & bien-être' => 'Santé & bien-être',
-                    'Sciences' => 'Sciences',
-                    'Sciences humaines & sociales' => 'Sciences humaines & sociales',
-                    'Sécurité' => 'Sécurité', 
-                    'Technique & industrie' => 'Technique & industrie',
-                    'Tourisme, sports & loisirs' => 'Tourisme, sports & loisirs',
-                    'Transports & logistique' => 'Transports & logistique'
+                    'Administration & législation' => 'Grande distribution',
+                    'Bâtiment & construction' => 'Vente & Commerce',
+                    'Communication' => 'Restauration',
+                    'Culture' => 'Artisanat',
+                    'Economie & gestion' => 'Marketing & Communication',
+                    'Environnement & nature' => 'Assistanat & secrétariat',
+                    'Hôtellerie & alimentation' => 'Immobilier',
                 ],
                 'choice_attr' => function($choice, $key, $value) {
                     return ['class' => 'attending_'.strtolower($key)];
@@ -43,19 +35,19 @@ class ProfileType extends AbstractType
 
             ->add('area', ChoiceType::class, [
                 'choices' => [
-                    'Auvergne-Rhône-Alpes' => 'Auvergne-Rhône-Alpes',
-                    'Bourgogne-Franche-Comté' => 'Bourgogne-Franche-Comté',
-                    'Bretagne' => 'Bretagne',
-                    'Centre-Val de Loire' => 'Centre-Val de Loire',
-                    'Corse' => 'Corse',
-                    'Grand Est' => 'Grand Est',
-                    'Hauts-de-France' => 'Hauts-de-France',
-                    'Île-de-France' => 'Île-de-France',
-                    'Normandie' => 'Normandie',
-                    'Nouvelle-Aquitaine' => 'Nouvelle-Aquitaine',
+                    // 'Auvergne-Rhône-Alpes' => 'Auvergne-Rhône-Alpes',
+                    // 'Bourgogne-Franche-Comté' => 'Bourgogne-Franche-Comté',
+                    // 'Bretagne' => 'Bretagne',
+                    // 'Centre-Val de Loire' => 'Centre-Val de Loire',
+                    // 'Corse' => 'Corse',
+                    // 'Grand Est' => 'Grand Est',
+                    // 'Hauts-de-France' => 'Hauts-de-France',
+                    // 'Île-de-France' => 'Île-de-France',
+                    // 'Normandie' => 'Normandie',
+                    // 'Nouvelle-Aquitaine' => 'Nouvelle-Aquitaine',
                     'Occitanie' => 'Occitanie',
-                    'Pays de la Loire' => 'Pays de la Loire', 
-                    'Provence-Alpes-Côte d\'Azur' => 'Provence-Alpes-Côte d\'Azur'
+                    // 'Pays de la Loire' => 'Pays de la Loire', 
+                    // 'Provence-Alpes-Côte d\'Azur' => 'Provence-Alpes-Côte d\'Azur'
                 ],
                 'choice_attr' => function($choice, $key, $value) {
                     return ['class' => 'attending_'.strtolower($key)];
