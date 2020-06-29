cf3bd54e9218fa647716e78d03e45caf3e63690a
skergoat <skergoatweb@gmail.com>
2020-06-29 14:18:28 +0700

    demo messages

--- assets/js/Student/component/doc.js
+++ assets/js/Student/component/doc.js
@@ -1,9 +1,23 @@
+// go and back 
+function back() {
+    $('#return-page').click(function() {
+        let data = $('#return-page').attr('data-url');
+        $('#input').prop("checked", false);
+        $('#test-1 #container-ajax').load('/student/load/' + data, further);
+    }); 
+}
+
+function further() {
+    $('#following-page').click(function() {
+        let data = $('#following-page').attr('data-url');
+        $('#input').prop( "checked", false);
+        $('#test-1 #container-ajax').load('/student/load/' + data, back);
+    });
+}
+
 // load new page 
-$('#following-page').click(function() {
-    let data = $('#following-page').attr('data-url');
-    $('#input').prop( "checked", false);
-    $('#test-1 #container-ajax').load('/student/load/' + data);
-});
+further();
+
 // stop program 
 $('#input').change(function() {
     let data = $(this).attr('data-url');
