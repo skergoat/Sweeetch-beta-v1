6dba0df4d182fa031bb4fcf42b2526b3c6806ade
skergoat <skergoatweb@gmail.com>
2020-06-29 15:48:41 +0700

    sdf

--- assets/js/Student/component/doc.js
+++ assets/js/Student/component/doc.js
@@ -1,22 +1,20 @@
 // go and back 
-function back() {
+function furtherBack() {
     $('#return-page').click(function() {
         let data = $('#return-page').attr('data-url');
         $('#input').prop("checked", false);
-        $('#test-1 #container-ajax').load('/student/load/' + data, further);
+        $('#test-1 #container-ajax').load('/student/load/' + data, furtherBack);
     }); 
-}
 
-function further() {
     $('#following-page').click(function() {
         let data = $('#following-page').attr('data-url');
         $('#input').prop( "checked", false);
-        $('#test-1 #container-ajax').load('/student/load/' + data, back);
+        $('#test-1 #container-ajax').load('/student/load/' + data, furtherBack);
     });
 }
 
 // load new page 
-further();
+furtherBack();
 
 // stop program 
 $('#input').change(function() {
